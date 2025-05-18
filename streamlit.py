a744031905dec053ad51e6c5a5602034af738aee
Nathan Stacey <6125085+nathanrstacey@users.noreply.github.com>
2025-05-18 18:43:26 -0400

    testing

--- streamlit.py
+++ streamlit.py
@@ -19,7 +19,7 @@ es = Elasticsearch(
 
 # Query Elasticsearch: Get top 5 most frequent `username_custom`
 query = {
-    "size": 0,
+    "size": 5,
     "aggs": {
         "top_usernames": {
             "terms": {
